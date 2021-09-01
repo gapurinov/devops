4e8fb8853ab94acec26e44b5c28cec7833244552
Yerzhan <gapurinoverzhan@gmail.com>
2021-09-01 16:00:48 +0300

    Workflow

--- app_python/tests/test_1.py
+++ app_python/tests/test_1.py
@@ -4,5 +4,5 @@ from main import simple_app
 client = simple_app.test_client()
 
 def test_response_code():
-    r = client.get("http://127.0.0.1:5000")
+    r = client.get("http://127.0.0.1:5000/")
     assert r.status_code == 200
--- app_python/tests/test_2.py
+++ app_python/tests/test_2.py
@@ -5,4 +5,4 @@ client = simple_app.test_client()
 
 def test_response_not_empty():
     r = client.get("http://127.0.0.1:5000")
-    assert r.content
+    assert r
