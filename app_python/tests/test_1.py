38975c8bb47fa09e2b753ea6c0bf0a93eff6b688
Yerzhan <gapurinoverzhan@gmail.com>
2021-09-01 15:54:30 +0300

    Workflow

--- app_python/tests/test_1.py
+++ app_python/tests/test_1.py
@@ -1,6 +1,8 @@
 import requests
+from main import simple_app
 
+client = simple_app.test_client()
 
 def test_response_code():
-    r = requests.get("http://127.0.0.1:5000")
+    r = client.get("http://127.0.0.1:5000")
     assert r.status_code == 200
